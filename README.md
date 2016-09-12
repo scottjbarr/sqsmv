86f1e6df35c0e5bc1748d541f04fbd0fe31154d7
Scott Barr <scottjbarr@gmail.com>
2016-09-12 21:02:26 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# sqsmv
+
+Move all messages from one SQS queue, to another.
+
+
+## Configuration
+
+The `AWS_SECRET_ACCESS_KEY`, `AWS_ACCESS_KEY_ID`, and ,`AWS_REGION`
+environment variables must be set.
+
+
+## Usage
+
+    sqsmv -src blah -dest blah
+
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2016 Scott Barr
+
+See [LICENSE.md](LICENSE.md)
---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+func main() {
+	src := flag.String("src", "", "source queue")
+	dest := flag.String("dest", "", "destination queue")
+	flag.Parse()
+
+	if *src == "" || *dest == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if os.Getenv("AWS_REGION") == "" {
+		fmt.Printf("AWS_REGION not set")
+		os.Exit(1)
+	}
+
+	region := os.Getenv("AWS_REGION")
+
+	log.Printf("source queue : %v", *src)
+	log.Printf("destination queue : %v", *dest)
+
+	config := &aws.Config{
+		Region: &region,
+	}
+
+	client := sqs.New(session.New(), config)
+
+	maxMessages := int64(10)
+	waitTime := int64(0)
+
+	rmin := &sqs.ReceiveMessageInput{
+		QueueUrl:            src,
+		MaxNumberOfMessages: &maxMessages,
+		WaitTimeSeconds:     &waitTime,
+	}
+
+	// loop as long as there are messages on the queue
+	for {
+		resp, err := client.ReceiveMessage(rmin)
+
+		if err != nil {
+			panic(err)
+		}
+
+		if len(resp.Messages) == 0 {
+			log.Printf("done")
+			return
+		}
+
+		log.Printf("received %v messages...", len(resp.Messages))
+
+		var wg sync.WaitGroup
+		wg.Add(len(resp.Messages))
+
+		for _, m := range resp.Messages {
+			go func(m *sqs.Message) {
+				defer wg.Done()
+
+				// write the message to the destination queue
+				smi := sqs.SendMessageInput{
+					MessageBody: m.Body,
+					QueueUrl:    dest,
+				}
+
+				_, err := client.SendMessage(&smi)
+
+				if err != nil {
+					log.Printf("ERROR sending message to destination %v", err)
+					return
+				}
+
+				// message was sent, dequeue from source queue
+				dmi := &sqs.DeleteMessageInput{
+					QueueUrl:      src,
+					ReceiptHandle: m.ReceiptHandle,
+				}
+
+				if _, err := client.DeleteMessage(dmi); err != nil {
+					log.Printf("ERROR dequeueing message ID %v : %v",
+						*m.ReceiptHandle,
+						err)
+				}
+			}(m)
+		}
+
+		// wait for all jobs from this batch...
+		wg.Wait()
+	}
+}
