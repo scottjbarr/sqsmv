0d9bfa30621e0cfea4f36861824c157c655eb37f
Brian Rice <brice@uplandsoftware.com>
2021-08-05 08:31:21 -0700

    support a -numClients CLI parameter and the use of parallelizer to run jobs in parallel

--- README.md
+++ README.md
@@ -28,6 +28,9 @@ Supply source and destination URL endpoints.
 
     sqsmv -src https://region.queue.amazonaws.com/123/queue-a -dest https://region.queue.amazonaws.com/123/queue-b
 
+To run jobs in parallel, use -numClients parameter:
+
+    sqsmv -src https://region.queue.amazonaws.com/123/queue-a -dest https://region.queue.amazonaws.com/123/queue-b -numClients 8
 
 ## Seeing is believing :)
 
--- main.go
+++ main.go
@@ -9,48 +9,73 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
+
+	"github.com/shomali11/parallelizer"
 )
 
 func main() {
 	src := flag.String("src", "", "source queue")
 	dest := flag.String("dest", "", "destination queue")
+	numClients := flag.Int("numClients", 1, "number of clients")
 	flag.Parse()
 
-	if *src == "" || *dest == "" {
+	if *src == "" || *dest == "" || *numClients < 1 {
 		flag.Usage()
 		os.Exit(1)
 	}
 
 	log.Printf("source queue : %v", *src)
 	log.Printf("destination queue : %v", *dest)
+	log.Printf("number of clients : %v", *numClients)
 
 	// enable automatic use of AWS_PROFILE like awscli and other tools do.
 	opts := session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}
 
-	session, err := session.NewSessionWithOptions(opts)
+	newSession, err := session.NewSessionWithOptions(opts)
 	if err != nil {
 		panic(err)
 	}
-
-	client := sqs.New(session)
 
 	maxMessages := int64(10)
 	waitTime := int64(0)
 	messageAttributeNames := aws.StringSlice([]string{"All"})
 
-	rmin := &sqs.ReceiveMessageInput{
+	rMin := &sqs.ReceiveMessageInput{
 		QueueUrl:              src,
 		MaxNumberOfMessages:   &maxMessages,
 		WaitTimeSeconds:       &waitTime,
 		MessageAttributeNames: messageAttributeNames,
 	}
 
+	if *numClients > 1 {
+		group := parallelizer.NewGroup(parallelizer.WithPoolSize(*numClients))
+		defer group.Close()
+		for i := 1; i <= *numClients; i++ {
+			group.Add(func() {
+				transferMessages(newSession, rMin, dest)
+			})
+		}
+		err = group.Wait()
+	} else {
+		transferMessages(newSession, rMin, dest)
+	}
+
+	log.Println("all done")
+	if err != nil {
+		log.Printf("error: %v", err)
+	}
+}
+
+//transferMessages loops, transferring a number of messages from the src to the dest at an interval.
+func transferMessages(theSession *session.Session, rMin *sqs.ReceiveMessageInput, dest *string) {
+	client := sqs.New(theSession)
+
 	lastMessageCount := int(1)
 	// loop as long as there are messages on the queue
 	for {
-		resp, err := client.ReceiveMessage(rmin)
+		resp, err := client.ReceiveMessage(rMin)
 
 		if err != nil {
 			panic(err)
@@ -88,7 +113,7 @@ func main() {
 
 				// message was sent, dequeue from source queue
 				dmi := &sqs.DeleteMessageInput{
-					QueueUrl:      src,
+					QueueUrl:      rMin.QueueUrl,
 					ReceiptHandle: m.ReceiptHandle,
 				}
 
